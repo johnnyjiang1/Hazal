f449503bda7a1ef61574cf949f0b49bc9d96fe80
johnnyjiang1 <jtc20000729@gmail.com>
2023-02-24 08:21:45 +0800

    Add a test window to Sandbox

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -14,9 +14,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		// ImGui::Begin("Test");
-		// ImGui::Text("Hello World");
-		// ImGui::End();
+		ImGui::Begin("Test");
+		ImGui::Text("Hello World");
+		ImGui::End();
 	}
 
 	void OnEvent(Hazal::Event& event) override
