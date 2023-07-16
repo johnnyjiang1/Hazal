5d853b38f974a9ec087b73757f595cc37da87cd2
johnnyjiang1 <jtc20000729@gmail.com>
2023-07-16 22:23:33 +0800

    Single Shader 2D Renderer

--- Hazal/src/Hazal/Renderer/Texture.h
+++ Hazal/src/Hazal/Renderer/Texture.h
@@ -13,6 +13,8 @@ namespace Hazal {
 		virtual uint32_t GetWidth() const = 0;
 		virtual uint32_t GetHeight() const = 0;
 		
+		virtual void SetData(void* data, uint32_t size) = 0;
+		
 		virtual void Bind(uint32_t slot = 0) const = 0;
 	};
 
@@ -20,6 +22,7 @@ namespace Hazal {
 	{
 	public:
 		virtual ~Texture2D() = default;
+		static Ref<Texture2D> Create(uint32_t width, uint32_t height);
 		static Ref<Texture2D> Create(const std::string& path);
 	};
 
